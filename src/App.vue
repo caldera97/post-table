8906cdbbe2f5fb23a53eed3f74e990582665c1cc
David Caldera <davidcaldera13@yahoo.com>
2022-09-14 15:21:50 -0500

    removed console log from app

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,6 @@ export default {
     async fetchPosts() {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
       const data = await res.json();
-      console.log(data);
       return data;
     },
   },
